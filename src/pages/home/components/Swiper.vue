1dce5ee5144770ae6f6d296d1e6e80d467791ec7
kalirfi <dongselfless@163.com>
2018-10-12 16:55:07 +0800

    homeswiper done

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl">
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1809/ff/1bf7d3a4f6591002.jpg_750x200_40809f2a.jpg'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1809/d4/e298dff35d3af802.jpg_750x200_9b0d56c1.jpg'
+      }, {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1809/8a/ef38d29f2791f02.jpg_750x200_c2defee1.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+      background: #fff !important
+  .wrapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 25%
+    bgcolor: #eee
+    .swiper-img
+      width: 100%
+</style>
