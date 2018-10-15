c8a9d02c12965aab6c4c6e6814d91a686d599dd5
kalirfi <dongselfless@163.com>
2018-10-15 20:10:01 +0800

    detail-banner and detail-header done

---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+      <div class="banner" @click="handleBannerClick()">
+          <img class="banner-img" src=""/>
+          <div class="banner-info">
+            <div class="banner-title">11111111
+            </div>
+            <div class="banner-number">
+              <span class="iconfont banner-icon">&#xe67b;</span>39
+            </div>
+          </div>
+      </div>
+      <common-gallary :imgs="imgs" v-show="showGallary"
+       @close="handleGallaryClose()"
+      ></common-gallary>
+  </div>
+</template>
+
+<script>
+import CommonGallary from 'common/gallary/Gallary'
+export default {
+  name: 'DetailBanner',
+  data () {
+    return {
+      imgs: [],
+      showGallary: false
+    }
+  },
+  components: {
+    CommonGallary
+  },
+  methods: {
+    handleBannerClick () {
+      this.showGallary = true
+    },
+    handleGallaryClose () {
+      this.showGallary = false
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .banner
+    background: blue
+    position: relative
+    overflow: hidden
+    height: 0
+    padding-bottom: 55%
+    .banner-img
+      width: 100%
+    .banner-info
+      display: flex
+      position: absolute
+      left: 0
+      right: 0
+      bottom: 0
+      line-height: .6rem
+      color: #fff
+      background-image: linear-gradient(top, rgba(0, 0, 0, 0),
+      rgba(0, 0, 0, 0.8))
+      .banner-title
+        flex: 1
+        font-size: .32rem
+        padding: 0 .2rem
+      .banner-number
+        margin-top: .14rem
+        padding: 0 .4rem
+        line-height: .32rem
+        height: .32rem
+        border-radius: .2rem
+        background: rgba(0, 0, 0, .8)
+        font-size: .24rem
+        .banner-icon
+          font-size: .24rem
+</style>
